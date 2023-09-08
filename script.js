c071689ad57992bb03fc92c9c1c16b12589ae196
Matt Marini <mattmarini8765@gmail.com>
2023-09-08 18:30:53 +1000

    Finished form

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-function validate_password() {
- 
-  var pass = document.getElementById('password').value;
-  var confirm_pass = document.getElementById('confirm_password').value;
-  if (pass != confirm_pass) {
-      document.getElementById('wrong_pass_alert').style.color = 'red';
-      document.getElementById('wrong_pass_alert').innerHTML
-          = 'Use same password';
-      document.getElementById('create').disabled = true;
-      document.getElementById('create').style.opacity = (0.4);
-  } else {
-      document.getElementById('wrong_pass_alert').style.color = 'green';
-      document.getElementById('wrong_pass_alert').innerHTML =
-          'Password Matched';
-      document.getElementById('create').disabled = false;
-      document.getElementById('create').style.opacity = (1);
+function Validate() {
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirm_password").value;
+  if (password != confirmPassword) {
+      alert("You first Passwords is not similar with 2nd password. Please enter same password in both");
+      return false;
   }
-}
-
-function wrong_pass_alert() {
-  if (document.getElementById('password').value != "" &&
-      document.getElementById('confirm_password').value != "") {
-      alert("Your response is submitted");
-  } else {
-      alert("Please fill all the fields");
-  }
+  return true;
 }
